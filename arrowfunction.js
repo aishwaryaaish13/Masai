8d723f146c1fa7201ed78e5e79713327842c7693
aishwaryaaish13 <your.email@example.com>
2025-11-24 22:20:19 +0530

    arrowfunction

---
+++ arrowfunction.js
@@ -0,0 +1,24 @@
+
+
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(4)); 
+console.log(isEven(7)); 
+
+
+
+
+
+let marks = 40;
+let result = marks >= 35 ? "Pass" : "Fail";
+
+console.log(result);
+
+
+
+
+
+const greet = name => `Hello, ${name ? name : "Guest"}`;
+
+console.log(greet("Aish"));
+console.log(greet());
