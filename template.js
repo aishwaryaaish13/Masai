673f6bb07ccd22f91444d9f2620077b488204fe7
aishwaryaaish13 <your.email@example.com>
2025-11-24 22:17:53 +0530

    template

---
+++ template.js
@@ -0,0 +1,29 @@
+
+
+const username = "Aish";
+const course = "JavaScript";
+
+console.log(`Hello ${username}, welcome to the ${course} course!`);
+
+
+
+
+const name = "Sam";
+const age = 21;
+
+const student = {
+  name,
+  age,
+  greet() {
+    console.log("Hello");
+  }
+};
+
+student.greet();
+
+
+
+
+const getFullName = (first, last) => `${first} ${last}`;
+
+console.log(getFullName("Aishwarya", "Choubey"));
